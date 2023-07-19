bde0f2ca312a4b72c5b823542d8151f48c745196
naveen3607 <naveenreddykallam3607@gmail.com>
2023-07-19 11:58:34 -0400

    Frontend shell script

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,14 @@
+#Frontend is the service to serve the web content over nginx web server.
 yum install nginx -y
-
+#nginx reverse proxy configuration
 cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-
+#Remove the default content that web server is serving
 rm -rf /usr/share/nginx/html/*
-
+#Download the frontend content
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-
-cd /usr/share/nginx/html
-
+#Extract the frontend content to web server
+cd /usr/share/nginx/html || return
 unzip /tmp/frontend.zip
-
-cd /
-
+#Start & Enable the nginx service
 systemctl enable nginx
 systemctl restart nginx
