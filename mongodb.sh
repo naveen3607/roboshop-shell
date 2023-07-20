9fa4a7d077b76b79aede9aa7fc5da43bb10f77c7
Kallam Naveen Reddy <naveenreddykallam3607@gmail.com>
2023-07-20 02:12:04 -0400

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -3,6 +3,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 #Install MongoDB
 yum install mongodb-org -y
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongo.conf
 #Enable & Start MongoDB
 systemctl enable mongod
 systemctl restart mongod
