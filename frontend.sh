adcd7552d62b2279fb157ef7ba8dcd6b8c53a853
naveen3607 <naveenreddykallam3607@gmail.com>
2023-07-21 12:30:23 -0400

    Shell Script

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,19 @@
 #Frontend is the service to serve the web content over nginx web server.
-yum install nginx -y
-#nginx reverse proxy configuration
-cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e\[31m<<<<<<<<<<<<Installing nginx>>>>>>>>>>>>\e[0m"
+yum install nginx -y &>>/tmp/roboshop.log
+echo -e "\e\[32m<<<<<<<<<<<<configuration file copied>>>>>>>>>>>>\e[0m"
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 #Remove the default content that web server is serving
-rm -rf /usr/share/nginx/html/*
+echo -e "\e\[33m<<<<<<<<<<<<Removed existing content>>>>>>>>>>>>\e[0m"
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 #Download the frontend content
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "\e\[34m<<<<<<<<<<<<Downloading frontend content>>>>>>>>>>>>\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 #Extract the frontend content to web server
-cd /usr/share/nginx/html || return
-unzip /tmp/frontend.zip
+echo -e "\e\[35m<<<<<<<<<<<<Extracting the frontend content>>>>>>>>>>>>\e[0m"
+cd /usr/share/nginx/html || return &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 #Start & Enable the nginx service
-systemctl enable nginx
-systemctl restart nginx
+echo -e "\e\[36m<<<<<<<<<<<<Started nginx service>>>>>>>>>>>>\e[0m"
+systemctl enable nginx &>>/tmp/roboshop.log
+systemctl restart nginx &>>/tmp/roboshop.log
