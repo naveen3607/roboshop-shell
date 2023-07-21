e07fcab3ada31380ff5265d3bb3dcad4c957b130
naveen3607 <naveenreddykallam3607@gmail.com>
2023-07-21 12:50:03 -0400

    Shell Script

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,13 @@
 #Creating the MongoDB repo
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[31m>>>>>>>>>>> Copied mongodb repo <<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 #Install MongoDB
-yum install mongodb-org -y
+echo -e "\e[32m>>>>>>>>>>> Installing mongodb <<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+yum install mongodb-org -y &>>/tmp/roboshop.log
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+echo -e "\e[33m>>>>>>>>>>> Updating ip in configuration file <<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/roboshop.log
 #Enable & Start MongoDB
-systemctl enable mongod
-systemctl restart mongod
+echo -e "\e[34m>>>>>>>>>>> Starting mongodb service <<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
