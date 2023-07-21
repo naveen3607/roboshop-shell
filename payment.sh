9161879116ff190c23e1da18301e6dda771d4c66
naveen3607 <naveenreddykallam3607@gmail.com>
2023-07-21 14:00:52 -0400

    Shell Script

--- payment.sh
+++ payment.sh
@@ -1,27 +1,27 @@
 log=/tmp/roboshop.log
 #Create payment service
 echo -e "\e[31m>>>>>>>>>>> Creating shipping service <<<<<<<<<<\e[0m" | tee -a ${log}
-cp payment.service /etc/systemd/system/payment.service
+cp payment.service /etc/systemd/system/payment.service &>> ${log}
 #Install python
 echo -e "\e[32m>>>>>>>>>>> Installing python <<<<<<<<<<\e[0m" | tee -a ${log}
-yum install python36 gcc python3-devel -y
+yum install python36 gcc python3-devel -y &>> ${log}
 #Add user, create directory, download payment content, install dependencies
 echo -e "\e[33m>>>>>>>>>>> Creating user <<<<<<<<<<\e[0m" | tee -a ${log}
-useradd roboshop
+useradd roboshop &>> ${log}
 echo -e "\e[34m>>>>>>>>>>> Removing existing directory <<<<<<<<<<\e[0m" | tee -a ${log}
-rm -rf /app
+rm -rf /app &>> ${log}
 echo -e "\e[35m>>>>>>>>>>> Creating new directory <<<<<<<<<<\e[0m" | tee -a ${log}
-mkdir /app
+mkdir /app &>> ${log}
 echo -e "\e[36m>>>>>>>>>>> Downloading payment content <<<<<<<<<<\e[0m" | tee -a ${log}
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>> ${log}
 echo -e "\e[31m>>>>>>>>>>> Extracting payment content <<<<<<<<<<\e[0m" | tee -a ${log}
-cd /app || return
-unzip /tmp/payment.zip
+cd /app || return &>> ${log}
+unzip /tmp/payment.zip &>> ${log}
 echo -e "\e[32m>>>>>>>>>>> Installing dependencies <<<<<<<<<<\e[0m" | tee -a ${log}
-cd /app || return
-pip3.6 install -r requirements.txt
+cd /app || return &>> ${log}
+pip3.6 install -r requirements.txt &>> ${log}
 #Enable & Start the service
 echo -e "\e[33m>>>>>>>>>>> Starting payment service <<<<<<<<<<\e[0m" | tee -a ${log}
-systemctl daemon-reload
-systemctl enable payment
-systemctl restart payment
+systemctl daemon-reload &>> ${log}
+systemctl enable payment &>> ${log}
+systemctl restart payment &>> ${log}
