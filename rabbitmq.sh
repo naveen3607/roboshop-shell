ea31d204522a40719225a3a6a261d28cb42b8938
naveen3607 <naveenreddykallam3607@gmail.com>
2023-07-21 13:46:17 -0400

    Shell Script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,11 +1,16 @@
+log=/tmp/roboshop.log
 #Configure yum repos
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+echo -e "\e[31m>>>>>>>>>>> Creating rabbitmq repo <<<<<<<<<<\e[0m" | tee -a ${log}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> ${log}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> ${log}
 #Install rabbitmq
-yum install rabbitmq-server -y
+echo -e "\e[32m>>>>>>>>>>> Installing rabbitmq <<<<<<<<<<\e[0m" | tee -a ${log}
+yum install rabbitmq-server -y &>> ${log}
 #Set permissions
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+echo -e "\e[33m>>>>>>>>>>> Setup permissions <<<<<<<<<<\e[0m" | tee -a ${log}
+rabbitmqctl add_user roboshop roboshop123 &>> ${log}
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> ${log}
 #Enable & Start the service
-systemctl enable rabbitmq-server
-systemctl restart rabbitmq-server
+echo -e "\e[31m>>>>>>>>>>> Starting rabbitmq service <<<<<<<<<<\e[0m" | tee -a ${log}
+systemctl enable rabbitmq-server &>> ${log}
+systemctl restart rabbitmq-server &>> ${log}
