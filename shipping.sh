22e9cb33c70a44f787de2081c02bcebcb5a10de0
naveen3607 <naveenreddykallam3607@gmail.com>
2023-07-21 13:39:40 -0400

    Shell Script

--- shipping.sh
+++ shipping.sh
@@ -1,20 +1,33 @@
+log=/tmp/roboshop.log
 #Create shipping service
-cp shipping.service /etc/systemd/system/shipping.service
+echo -e "\e[31m>>>>>>>>>>> Creating shipping service <<<<<<<<<<\e[0m" | tee -a ${log}
+cp shipping.service /etc/systemd/system/shipping.service &>> ${log}
 #Install Maven
-yum install maven -y
+echo -e "\e[32m>>>>>>>>>>> Installing maven <<<<<<<<<<\e[0m" | tee -a ${log}
+yum install maven -y &>> ${log}
 #Add user, create directory, download shipping content, install dependencies
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app || return
-unzip /tmp/shipping.zip
-cd /app || return
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
+echo -e "\e[33m>>>>>>>>>>> Creating user <<<<<<<<<<\e[0m" | tee -a ${log}
+useradd roboshop &>> ${log}
+echo -e "\e[34m>>>>>>>>>>> Removing existing directory <<<<<<<<<<\e[0m" | tee -a ${log}
+rm -rf /app &>> ${log}
+echo -e "\e[35m>>>>>>>>>>> Creating new directory <<<<<<<<<<\e[0m" | tee -a ${log}
+mkdir /app &>> ${log}
+echo -e "\e[36m>>>>>>>>>>> Downloading shipping content <<<<<<<<<<\e[0m" | tee -a ${log}
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>> ${log}
+echo -e "\e[37m>>>>>>>>>>> Extracting shipping content <<<<<<<<<<\e[0m" | tee -a ${log}
+cd /app || return &>> ${log}
+unzip /tmp/shipping.zip &>> ${log}
+echo -e "\e[38m>>>>>>>>>>> Installing dependencies <<<<<<<<<<\e[0m" | tee -a ${log}
+cd /app || return &>> ${log}
+mvn clean package &>> ${log}
+mv target/shipping-1.0.jar shipping.jar &>> ${log}
 #Install mysql and load the schema
-yum install mysql -y
-mysql -h mysql.naveen3607.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+echo -e "\e[39m>>>>>>>>>>> Installing mysql <<<<<<<<<<\e[0m" | tee -a ${log}
+yum install mysql -y &>> ${log}
+echo -e "\e[31m>>>>>>>>>>> Loading schema <<<<<<<<<<\e[0m" | tee -a ${log}
+mysql -h mysql.naveen3607.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> ${log}
 #Enable & start the service
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+echo -e "\e[32m>>>>>>>>>>> Staring shipping service <<<<<<<<<<\e[0m" | tee -a ${log}
+systemctl daemon-reload &>> ${log}
+systemctl enable shipping &>> ${log}
+systemctl restart shipping &>> ${log}
